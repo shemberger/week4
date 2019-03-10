d9cea7645c660d1b2eb870154f76e7ff427a279d
shemberger <47642886+shemberger@users.noreply.github.com>
2019-03-10 16:37:44 -0400

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,3 @@
+SELECT * FROM car WHERE car='Mercury';
+UPDATE car SET Car='Dodge', Color='Blue', Year='1999' WHERE car='Mercury';
+UPDATE car SET Car='Ford', Color='Red', Year='2018' WHERE car='Honda';
